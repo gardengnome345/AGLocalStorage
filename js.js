2a32191a7c6875f956883717e51ae400127a7805
Alexander Garyaev <125agaryaev@frhsd.com>
2024-11-05 11:24:36 -0500

    finsihed

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 currentNum = 0;
-array = []
+
 function orangebutton()
 {
     document.getElementById("numDisplay").style.color = "orange";
@@ -24,12 +24,26 @@ function subtract()
 }
 function save()
 {
-    let object =
-        {
-            color: document.getElementById("savedNums").innerHTML,
-            number: document.getElementById("savedColor").innerHTML,
-        }
-        array.push(object);
-    array.push(document.getElementById("savedNums").innerHTML += currentNum);
+   let color = document.getElementById("numDisplay").style.color;
+   let number = currentNum;
+   let entry = {color: color, number: number};
+   localStorage.setItem(localStorage.length, JSON.stringify(entry));
+   let savedEntry = document.createElement("div");
+   savedEntry.innerHTML = number;
+   savedEntry.style.color = color;
+   document.getElementById("savedNums").appendChild(savedEntry);
 
+}
+function reload()
+{
+    document.getElementById("savedNums").innerHTML = "";
+    let numbers = localStorage.length;
+    for (let i = 0; i < numbers; i++)
+    {
+        let obj = JSON.parse(localStorage.getItem(i.toString()));
+        let div = document.createElement("div");
+        div.innerText = obj.number;
+        div.style.color = obj.color;
+        document.getElementById("savedNums").appendChild(div);
+    }
 }
